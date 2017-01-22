07127697716644c6087c4ccae8fb1d3c62ecf88f
Todd Harding <tharding@dundee.ac.uk>
2017-01-22 16:44:51 +0000

    created initial generate primes methods filled in test cases for generate primes

--- lib/epg.ex
+++ lib/epg.ex
@@ -61,4 +61,45 @@ defmodule Epg do
     {:error, "unable to parse input"}
   end
 
+  @doc """
+    Generates a list of prime numbers of given length
+
+    Returns `{:error, message}` if anything other than
+    a positive integer is entered
+
+    ## Examples
+
+      iex> Epg.generate_primes(2)
+      [2, 3]
+
+
+      iex> Epg.generate_primes(3)
+      [2, 3, 5]
+
+  """
+  def generate_primes(n) when is_integer(n) and n > 0 do
+    generate_primes_priv(n, 0, [])
+  end
+
+  def generate_primes(n) when n < 1 do
+    {:error, "input must be a positive integer"}
+  end
+
+  def generate_primes(n) when is_binary(n) do
+    {:error, "input must be a positive integer"}
+  end
+
+  defp generate_primes_priv(0, _highest_prime, primes) do
+    primes
+  end
+
+  defp generate_primes_priv(n, highest_number, primes) do
+    next_num = highest_number + 1
+    case is_prime(next_num) do
+      true -> generate_primes_priv(n - 1, next_num, primes ++ [next_num])
+      false -> generate_primes_priv(n, next_num, primes)
+    end
+  end
 end
+
+
--- test/generate_primes_test.exs
+++ test/generate_primes_test.exs
@@ -2,22 +2,32 @@ defmodule Epg.Test.GeneratePrimes do
   use ExUnit.Case
 
   test "if input is 1, the output must be [2]" do
-    assert false
+    assert Epg.generate_primes(1) == [2]
   end
 
   test "if input is 2, the output must be [2, 3]" do
-    assert false
+    assert Epg.generate_primes(2) == [2, 3]
   end
 
   test "if input is 3, the output must be [2, 3, 5]" do
-    assert false
+    assert Epg.generate_primes(3) == [2, 3, 5]
+  end
+
+  test "first 100 primes" do
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
+      107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+      233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367,
+      373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503,
+      509, 521, 523, 541]
+
+    assert Epg.generate_primes(100) == primes
   end
 
   test "generate_primes must output an error if given a negative number" do
-    assert false
+    assert {:error, _message} = Epg.generate_primes(-2)
   end
 
   test "input must not be a string" do
-    assert false
+    assert {:error, _message} = Epg.generate_primes("25")
   end
 end
