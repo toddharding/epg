e81f02292d594f4279a84b26d023074205347a58
Todd Harding <tharding@dundee.ac.uk>
2017-01-22 15:39:10 +0000

    created initial prime test methods filled in test cases for prime test

--- lib/epg.ex
+++ lib/epg.ex
@@ -26,4 +26,39 @@ defmodule Epg do
     {:error, "input must be a positive integer"}
   end
 
+  @doc """
+  Checks if number is prime
+
+  Returns true if prime false if not
+  Returns `{:error, message}` on failure
+  """
+  def is_prime(1) do
+    false
+  end
+
+  def is_prime(2) do
+    true
+  end
+
+  def is_prime(n) when is_integer(n) and n > 2 do
+    nums = 2..n-1
+    nums
+    |> Enum.map(fn x -> rem(n, x) end)
+    |> Enum.reduce_while(0, fn (i, acc) ->
+      if i == 0, do: {:halt, false}, else: {:cont, true}
+    end )
+  end
+
+  def is_prime(x) when is_binary(x) do
+    {:error, "input must be an integer"}
+  end
+
+  def is_prime(x) when is_integer(x) and x < 1 do
+    {:error, "input must be a positive integer"}
+  end
+
+  def is_prime(_) do
+    {:error, "unable to parse input"}
+  end
+
 end
--- test/is_prime_test.exs
+++ test/is_prime_test.exs
@@ -2,26 +2,26 @@ defmodule Epg.Test.IsPrime do
   use ExUnit.Case
 
   test "1 is not prime" do
-    assert false
+    assert Epg.is_prime(1) == false
   end
 
   test "2 is prime" do
-    assert false
+    assert Epg.is_prime(2) == true
   end
 
   test "3 is prime" do
-    assert false
+    assert Epg.is_prime(3) == true
   end
 
   test "4 is not prime" do
-    assert false
+    assert Epg.is_prime(4) == false
   end
 
   test "negative number returns an error" do
-    assert false
+    assert {:error, _message} = Epg.is_prime(-1)
   end
 
   test "input must not be a string" do
-    assert false
+    assert {:error, _message} = Epg.is_prime("51")
   end
 end
