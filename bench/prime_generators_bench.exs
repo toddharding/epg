150aaacb55b60545057a872ad1d12c55942b843f
Todd Harding <tharding@dundee.ac.uk>
2017-01-25 12:51:41 +0000

    Added a SOE using ETS for storage to compare storage performance against map %{}

--- bench/prime_generators_bench.exs
+++ bench/prime_generators_bench.exs
@@ -1,42 +1,67 @@
 defmodule Epg.Bench.PrimeGenerators do
   use Benchfella
 
-  # 10 prime numbers
-  bench "Naive prime generator first 10 prime numbers" do
-      Epg.generate_primes(10)
-    end
+#  # 10 prime numbers
+#  bench "Naive prime generator first 10 prime numbers" do
+#      Epg.generate_primes(10)
+#    end
+#
+#  bench "Naive Multi Threading first 10 prime numbers" do
+#    Epg.NMT.generate_primes(10)
+#  end
+#
+#  bench "Sieve of Eratosthenes first 10 prime numbers" do
+#    Epg.SOE.generate_primes(10)
+#  end
+#
+#  bench "MT Sieve of Eratosthenes first 10 prime numbers" do
+#    Epg.SOE.generate_primes(10)
+#  end
+#
+#  # 100 prime numbers
+#  bench "Naive prime generator first 100 prime numbers" do
+#    Epg.generate_primes(100)
+#  end
+#
+#  bench "Naive Multi Threading first 100 prime numbers" do
+#    Epg.NMT.generate_primes(100)
+#  end
+#
+#  bench "Sieve of Eratosthenes first 100 prime numbers" do
+#    Epg.SOE.generate_primes(100)
+#  end
+#
+#  bench "MT Sieve of Eratosthenes first 100 prime numbers" do
+#    Epg.SOE.generate_primes(100)
+#  end
 
-  bench "Naive Multi Threading first 10 prime numbers" do
-    Epg.NMT.generate_primes(10)
+#  # 1000 prime numbers
+#  bench "Naive prime generator first 1000 prime numbers" do
+#    Epg.generate_primes(1000)
+#  end
+#
+#  bench "Naive Multi Threading first 1000 prime numbers" do
+#    Epg.NMT.generate_primes(1000)
+#  end
+#
+#  bench "Sieve of Eratosthenes first 1000 prime numbers" do
+#    Epg.SOE.generate_primes(1000)
+#  end
+#
+#  bench "MT Sieve of Eratosthenes first 1000 prime numbers" do
+#    Epg.SOE.generate_primes(1000)
+#  end
+
+  # 10000
+  bench "Sieve of Eratosthenes first 10000 prime numbers" do
+    Epg.SOE.generate_primes(10000)
   end
 
-  bench "Sieve of Eratosthenes first 10 prime numbers" do
-    Epg.SOE.generate_primes(10)
+  bench "MT Sieve of Eratosthenes first 10000 prime numbers" do
+    Epg.SOEMT.generate_primes(10000)
   end
 
-  # 100 prime numbers
-  bench "Naive prime generator first 100 prime numbers" do
-    Epg.generate_primes(100)
-  end
-
-  bench "Naive Multi Threading first 100 prime numbers" do
-    Epg.NMT.generate_primes(100)
-  end
-
-  bench "Sieve of Eratosthenes first 100 prime numbers" do
-    Epg.SOE.generate_primes(100)
-  end
-
-  # 1000 prime numbers
-  bench "Naive prime generator first 1000 prime numbers" do
-    Epg.generate_primes(1000)
-  end
-
-  bench "Naive Multi Threading first 1000 prime numbers" do
-    Epg.NMT.generate_primes(1000)
-  end
-
-  bench "Sieve of Eratosthenes first 1000 prime numbers" do
-    Epg.SOE.generate_primes(1000)
+  bench "ETS Sieve of Eratosthenes first 10000 prime numbers" do
+    Epg.SOEETS.generate_primes(10000)
   end
 end
